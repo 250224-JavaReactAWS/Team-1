6dbd1e0dc162c087fbaf10479911fced690a62ba
Pfrigate <48607338+Pfrigate@users.noreply.github.com>
2025-04-01 10:52:51 -0600

    Add files via upload

---
+++ RevStay/src/main/java/com/Rev/RevStay/RevStayApplication.java
@@ -0,0 +1,13 @@
+package com.Rev.RevStay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevStayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RevStayApplication.class, args);
+	}
+
+}
---
+++ RevStay/src/test/java/com/Rev/RevStay/RevStayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Rev.RevStay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevStayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
