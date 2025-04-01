6b344ebc0e64bb8cb19d32d636404ba6965b3dc0
Agustín Aranda Cruz <66988375+Agustin-Aranda@users.noreply.github.com>
2025-04-01 15:00:50 -0600

    Update Favorite.java
    Adding the JPA configuration for Favorite

--- RevStay/src/main/java/com/Rev/RevStay/models/Favorite.java
+++ RevStay/src/main/java/com/Rev/RevStay/models/Favorite.java
@@ -1,4 +1,57 @@
 package com.Rev.RevStay.models;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "favorites")
+
 public class Favorite {
+    /* I have questions with this part
+    @EmbeddedId
+    private FavoriteId id;
+     */
+
+    //Maps the relations with user and hotel Ids
+    @ManyToOne
+    @MapsId("userId")
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @MapsId("hotelId")
+    @JoinColumn(name = "hotel_id", nullable = false)
+    private Hotel hotel;
+
+    // Constructor
+    public Favorite() {}
+
+//    public Favorite(User user, Hotel hotel) {
+//        this.user = user;
+//        this.hotel = hotel;
+//        this.id = new FavoriteId(user.getUserId(), hotel.getHotelId());
+//    }
+
+//    public FavoriteId getId() {
+//        return id;
+//    }
+//
+//    public void setId(FavoriteId id) {
+//        this.id = id;
+//    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Hotel getHotel() {
+        return hotel;
+    }
+
+    public void setHotel(Hotel hotel) {
+        this.hotel = hotel;
+    }
 }
